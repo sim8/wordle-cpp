bfeae5ee086b23f79ca1712002bc09de770cf76a
Simeon Lees <simeon.k.lees@gmail.com>
2024-04-21 20:42:53 +0100

    basic random word gen

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <vector>
 #include <ncurses.h>
+#include <string>
+
+std::vector<std::string> words = {"arise", "world", "drink", "happy"};
+
+std::string getRandomWord()
+{
+  int randomWordIndex = rand() % words.size();
+  return words.at(randomWordIndex);
+}
 
 int main()
 {
-  initscr();
+  initscr(); // must be called before any other routines
+  std::srand(std::time(0));
   printw("Hello wordle (with curses)!");
+  printw("\n"); // probs a better way to do this
+  printw(getRandomWord().c_str());
   refresh();
   getch();
-  endwin();
+  endwin(); // must be called before exiting
 
   return 0;
 }
